c20d43b35c006f9e787ceb25b13572be0d59a000
Dewmini Thakshila Kavindi <kavindi_adt@outlook.com>
2023-02-13 14:47:13 +0530

    Fixed intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,19 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import { remarkReadingTime } from "./src/utils/all";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://uokcs-19.github.io/",
   experimental: {
-    contentCollections: true,
+    contentCollections: true
   },
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: false
   },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()]
 });
